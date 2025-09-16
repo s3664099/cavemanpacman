cf2a5568e57919bf9b8534f9018b345dd4b0f19f
Dave Sarkies <s3664099@student.rmit.edu.au>
2025-09-16 17:42:57 +1000

    Added Deer Class

---
+++ deer.py
@@ -0,0 +1,29 @@
+"""
+File: Caveman Pacman Deer
+Author: David Sarkies
+Initial: 16 September 2025
+Update: 16 September 2025
+Version: 0.0
+"""
+
+class Deer:
+
+	def __init__(self,row,col):
+		self.position = (row,col)
+		self.running = False
+
+	def get_position:
+		return self.position
+
+	def check_position(self,row,col):
+		return (row,col) == self.position
+
+	def set_position(self,row,col):
+		self.position = (row,col)
+
+	
+
+
+"""
+16 September 2025 - Created file
+"""
