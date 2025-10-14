b6b79946fa931f0334ea4420fc90a3acf0e51749
Dave Sarkies <s3664099@student.rmit.edu.au>
2025-10-14 18:27:26 +1100

    Added non blocking pause

--- main.py
+++ main.py
@@ -29,18 +29,14 @@ def remove_deers(deers,player,bears):
                 any(deer.collides_with(bear) for bear in bears))
     ]
 
-def pause(action,player):
-    pause = False
-
+"""Non-blocking pause handling that returns new pause state"""
+def handle_pause(action, is_paused):
+    
     if action == "P":
-        pause = True
-    while(pause):
-        action = control.get_keypress()
-        if action == "P" or action == "Q":
-            pause = False
-        quit(action,player)
+        is_paused = not is_paused  # Toggle pause state
+    return is_paused
 
-def quit(action,player):
+def handle_quit(action,player):
     if action == "Q":
         player.set_running(False)
 
@@ -50,6 +46,7 @@ def main():
     game_screen = View(pacman_map.get_dimensions())
     game_screen.update_screen(pacman_map,0)
     base_time = time.time()
+    is_paused = False
 
     deers = []
     bears = []
@@ -65,36 +62,37 @@ def main():
 
     while(player.get_running()):
         action = control.get_keypress()
-        quit(action,player)
-        pause(action,player)
-        pacman_map.set_map(player.move_player(action,pacman_map.get_map(),pacman_map.get_width()))
+        handle_quit(action,player)
+        is_paused = handle_pause(action, is_paused)
         game_screen.update_screen(pacman_map,player.get_score())
         
-        current_time = time.time()
+        if not is_paused:
+            pacman_map.set_map(player.move_player(action,pacman_map.get_map(),pacman_map.get_width()))
+            current_time = time.time()
 
-        if (current_time-base_time>HALF_MOVE_INTERVAL) and (current_time-base_time<FULL_MOVE_INTERVAL):
-            for bear in bears:
-                if (bear.get_chasing()):
-                    pacman_map.set_map(bear.move_bear(pacman_map.get_map(),pacman_map.get_width(),pacman_map.get_entrance()))
-                    bear.set_chasing(False)
+            if (current_time-base_time>HALF_MOVE_INTERVAL) and (current_time-base_time<FULL_MOVE_INTERVAL):
+                for bear in bears:
+                    if (bear.get_chasing()):
+                        pacman_map.set_map(bear.move_bear(pacman_map.get_map(),pacman_map.get_width(),pacman_map.get_entrance()))
+                        bear.set_chasing(False)
 
-            for deer in deers:
-                if (deer.get_fleeing()):
+                for deer in deers:
+                    if (deer.get_fleeing()):
+                        pacman_map.set_map(deer.move_deer(pacman_map.get_map(),pacman_map.get_width()))
+                        deer.set_fleeing(False)
+
+
+                game_screen.update_screen(pacman_map,player.get_score())
+            elif (current_time-base_time>FULL_MOVE_INTERVAL):
+                base_time = current_time
+
+                for deer in deers:
                     pacman_map.set_map(deer.move_deer(pacman_map.get_map(),pacman_map.get_width()))
-                    deer.set_fleeing(False)
 
+                for bear in bears:
+                    pacman_map.set_map(bear.move_bear(pacman_map.get_map(),pacman_map.get_width(),pacman_map.get_entrance()))
 
-            game_screen.update_screen(pacman_map,player.get_score())
-        elif (current_time-base_time>FULL_MOVE_INTERVAL):
-            base_time = current_time
-
-            for deer in deers:
-                pacman_map.set_map(deer.move_deer(pacman_map.get_map(),pacman_map.get_width()))
-
-            for bear in bears:
-                pacman_map.set_map(bear.move_bear(pacman_map.get_map(),pacman_map.get_width(),pacman_map.get_entrance()))
-
-        deers = remove_deers(deers,player,bears)
+            deers = remove_deers(deers,player,bears)
 
     return player.get_score()
 
@@ -119,4 +117,5 @@ if __name__ == "__main__":
 12 October 2025 - Move code into main function
 13 October 2025 - Made deer move faster if fleeing
 14 October 2024 - Created constants and tightened remove deer function
+                - Added non blocking pause
 """
