95c52cc7a8c768abe2b00cf6e2ef98dab3eb14e0
David Sarkies <david.sarkies@internode.on.net>
2025-10-19 15:18:23 +1100

    Tightened code for control

--- control.py
+++ control.py
@@ -8,6 +8,19 @@ Version: 0.2
 
 import pygame
 
+KEY_BINDINGS = {
+	pygame.K_UP: "N",
+	pygame.K_w: "N",
+	pygame.K_DOWN: "S",
+	pygame.K_s:"S",
+	pygame.K_LEFT:"W",
+	pygame.K_a:"W",
+	pygame.K_RIGHT:"E",
+	pygame.K_d:"E",
+	pygame.K_q:"Q",
+	pygame.K_p:"P"
+}
+
 def get_keypress():
 
 	events = pygame.event.get()
@@ -15,18 +28,7 @@ def get_keypress():
 
 	for event in events:
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP or event.key == pygame.K_w:
-				keys.append("N")
-			elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-				keys.append("S")
-			elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-				keys.append("W")
-			elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-				keys.append("E")
-			elif event.key == pygame.K_q:
-				keys.append("Q")
-			elif event.key == pygame.K_p:
-				keys.append("P")
+			keys.append(KEY_BINDINGS.get(event.key,""))
 
 	return keys[-1] if keys else ""
 
@@ -35,4 +37,5 @@ def get_keypress():
 9 September 2025 - Created file
 11 October 2025 - Added pause keystroke
 19 October 2025 - Added handling for multiple keystrokes
+				- Tightened code
 """
